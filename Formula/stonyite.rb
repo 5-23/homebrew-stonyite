62cff29629c98ad94d5784de50ec2c8f354c835d
5-23 dev <86705803+5-23@users.noreply.github.com>
2024-03-12 20:11:27 +0900

    Update stonyite.rb

--- Formula/stonyite.rb
+++ Formula/stonyite.rb
@@ -2,7 +2,7 @@ class Stonyite < Formula
     desc "A eng-kor dictionary for the terminal users"
     homepage "https://github.com/5-23/stonyite"
     url "https://github.com/5-23/stonyite/releases/download/v0.1.1/stonyite.tar.gz"
-    sha256 "f8c313eb0db8321cfd244fc69e6f826fbf633783dd23922de2bb221c5e41d526"
+    sha256 "e78038574d5f6cd3f0d4ae73331df3852915dd71ba0ef34f4a7e79f6ea2cb249"
     version "0.1.1"
 
     def install
