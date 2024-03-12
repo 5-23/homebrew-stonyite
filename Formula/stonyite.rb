61a04e1dfbb3ca72dc2c234a9023191c6d780715
5-23 dev <86705803+5-23@users.noreply.github.com>
2024-03-12 20:04:58 +0900

    fix: readme add bug

--- Formula/stonyite.rb
+++ Formula/stonyite.rb
@@ -1,9 +1,9 @@
 class Stonyite < Formula
     desc "A eng-kor dictionary for the terminal users"
     homepage "https://github.com/5-23/stonyite"
-    url "https://github.com/5-23/stonyite/releases/download/v0.1.0/stonyite.tar.gz"
-    sha256 "a25ccbf9083ab4e1234a36e39d74691e46c9dc01cf761519f358b193d175a825"
-    version "0.1.0"
+    url "https://github.com/5-23/stonyite/releases/download/v0.1.1/stonyite.tar.gz"
+    sha256 "f8c313eb0db8321cfd244fc69e6f826fbf633783dd23922de2bb221c5e41d526"
+    version "0.1.1"
 
     def install
       bin.install "stonyite"
